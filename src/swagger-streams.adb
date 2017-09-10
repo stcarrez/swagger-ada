8e060fc645e7b4f6e1e1093efd33bf66067650fa
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-09-10 16:18:20 +0200

    Implement the Serialize procedure for the JSON/XML serialization

---
+++ src/swagger-streams.adb
@@ -0,0 +1,44 @@
+-----------------------------------------------------------------------
+--  swagger-streams -- Stream operations
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+package body Swagger.Streams is
+
+   --  ------------------------------
+   --  Serialize a list of strings in the stream.
+   --  ------------------------------
+   procedure Serialize (Stream : in out Output_Stream'Class;
+                        Name   : in String;
+                        Value  : in Swagger.UString_Vectors.Vector) is
+   begin
+      Stream.Start_Array (Name);
+      for S of Value loop
+         Stream.Write_Entity ("", S);
+      end loop;
+      Stream.End_Array (Name);
+   end Serialize;
+
+   --  ------------------------------
+   --  Serialize a long value.
+   --  ------------------------------
+   procedure Serialize (Stream : in out Output_Stream'Class;
+                        Name   : in String;
+                        Value  : in Swagger.Long) is
+   begin
+      Stream.Write_Long_Entity (Name, Value);
+   end Serialize;
+
+end Swagger.Streams;
