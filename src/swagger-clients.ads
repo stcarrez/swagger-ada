06105b051f584b9b88c2c1761491427895f9f873
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-09-16 16:19:13 +0200

    Define the Swagger.Clients package with some operations to be used by the generated code

---
+++ src/swagger-clients.ads
@@ -0,0 +1,70 @@
+-----------------------------------------------------------------------
+--  swagger-clients -- Rest client support
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Util.Http.Clients;
+package Swagger.Clients is
+
+   type Request_Type is record
+      S : Natural := 0;
+   end record;
+
+   type Operation_Type is (GET, POST, PUT, DELETE);
+
+   type URI_Type is tagged private;
+
+   --  Set the path to use for the URI.
+   procedure Set_Path (URI  : in out URI_Type;
+                       Path : in String);
+
+   --  Set the path parameter.
+   procedure Set_Path_Param (URI   : in out URI_Type;
+                             Name  : in String;
+                             Value : in String);
+
+   --  Set the path parameter.
+   procedure Set_Path_Param (URI   : in out URI_Type;
+                             Name  : in String;
+                             Value : in UString);
+
+   --  Add a query parameter.
+   procedure Add_Param (URI   : in out URI_Type;
+                        Name  : in String;
+                        Value : in String);
+
+   --  Add a query parameter.
+   procedure Add_Param (URI   : in out URI_Type;
+                        Name  : in String;
+                        Value : in UString);
+
+   type Client_Type is new Util.Http.Clients.Client with null record;
+
+   procedure Call (Client    : in out Client_Type;
+                   Operation : in Operation_Type;
+                   URI       : in URI_Type'Class;
+                   Request   : in Request_Type);
+
+   procedure Call (Client    : in out Client_Type;
+                   Operation : in Operation_Type;
+                   URI       : in URI_Type'Class);
+
+private
+
+   type URI_Type is tagged record
+      URI : UString;
+   end record;
+
+end Swagger.Clients;
