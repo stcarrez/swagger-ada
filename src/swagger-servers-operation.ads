0e785520aed2ee95674e04380ac04b52434c963d
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-12-09 22:38:32 +0100

    New package for the declaration of a REST operation

---
+++ src/swagger-servers-operation.ads
@@ -0,0 +1,31 @@
+-----------------------------------------------------------------------
+--  swagger-server-operation -- Rest server operation
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Security.Permissions;
+generic
+   with procedure Handler (Req     : in out Swagger.Servers.Request'Class;
+                           Reply   : in out Swagger.Servers.Response'Class;
+                           Stream  : in out Swagger.Servers.Output_Stream'Class;
+                           Context : in out Swagger.Servers.Context_Type);
+   Method     : Method_Type := ASF.Rest.GET;
+   URI        : String;
+   Permission : Security.Permissions.Permission_Index := Security.Permissions.NONE;
+package Swagger.Servers.Operation is
+
+   function Definition return Descriptor_Access;
+
+end Swagger.Servers.Operation;
