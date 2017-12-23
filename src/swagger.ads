bfc41bfb5bdce93c75af48990fa1d15867c20943
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-12-23 16:19:38 +0100

    Declare the Nullable_Integer type

--- src/swagger.ads
+++ src/swagger.ads
@@ -59,6 +59,7 @@ package Swagger is
 
    subtype Long is Long_Long_Integer;
    subtype Nullable_Long is Util.Nullables.Nullable_Long;
+   subtype Nullable_Integer is Util.Nullables.Nullable_Integer;
 
    subtype Http_Content_Type is UString;
 
