c2398d63e068632467c6ff6276f91e6f6429c1f4
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-12-24 15:51:57 +0100

    Implement the Deserialize for a Nullable_Integer

--- src/swagger-streams.adb
+++ src/swagger-streams.adb
@@ -134,6 +134,23 @@ package body Swagger.Streams is
       Value := Util.Beans.Objects.To_Integer (Item);
    end Deserialize;
 
+   procedure Deserialize (From  : in Swagger.Value_Type;
+                          Name  : in String;
+                          Value : out Nullable_Integer) is
+      Item : Swagger.Value_Type;
+   begin
+      if Name = "" then
+         Item := From;
+      else
+         Deserialize (From, Name, Item);
+      end if;
+      if Util.Beans.Objects.Is_Null (Item) then
+         Value := (Is_Null => True, Value => <>);
+      else
+         Value := (Is_Null => False, Value => Util.Beans.Objects.To_Integer (Item));
+      end if;
+   end Deserialize;
+
    --  ------------------------------
    --  Extract an integer value stored under the given name.
    --  ------------------------------
