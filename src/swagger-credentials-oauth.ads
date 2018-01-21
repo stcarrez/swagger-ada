79157204b5228c031f1913a2bcff1c68dffcf2fd
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-01-21 22:01:54 +0100

    Declare Request_Token and Refresh_Token procedures

--- src/swagger-credentials-oauth.ads
+++ src/swagger-credentials-oauth.ads
@@ -28,11 +28,23 @@ package Swagger.Credentials.OAuth is
    procedure Set_Credentials (Credential : in OAuth2_Credential_Type;
                               Into       : in out Util.Http.Clients.Client'Class);
 
+   --  Request a OAuth token with username and password credential.
+   --  Upon successful completion, the credential contains an access token that
+   --  can be used to authorize REST operations.
+   procedure Request_Token (Credential : in out OAuth2_Credential_Type;
+                            Username   : in String;
+                            Password   : in String;
+                            Scope      : in String);
+
+   --  Refresh the OAuth access token with the refresh token.
+   procedure Refresh_Token (Credential : in out OAuth2_Credential_Type);
+
 private
 
    type OAuth2_Credential_Type is new Security.OAuth.Clients.Application
      and Credential_Type with record
-      Token  : Security.OAuth.Clients.Access_Token_Access;
+      Token  : Security.OAuth.Clients.Grant_Type;
+      Scope  : Ada.Strings.Unbounded.Unbounded_String;
    end record;
 
 end Swagger.Credentials.OAuth;
