04e4788beed821b094bacce4db828ec3820e3c76
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-03-04 10:06:39 +0100

    Revert previous commit

--- regtests/server/src/testapi-server.adb
+++ regtests/server/src/testapi-server.adb
@@ -2,17 +2,23 @@ with Ada.IO_Exceptions;
 with AWS.Config.Set;
 with Swagger.Servers.AWS;
 with Swagger.Servers.Applications;
+with Util.Strings;
 with Util.Log.Loggers;
 with Util.Properties;
+with Util.Properties.Basic;
 with TestAPI.Servers;
 procedure TestAPI.Server is
+   use Util.Properties.Basic;
+
    procedure Configure (Config : in out AWS.Config.Object);
 
    CONFIG_PATH  : constant String := "testapi.properties";
 
+   Port : Natural := 8080;
+
    procedure Configure (Config : in out AWS.Config.Object) is
    begin
-      AWS.Config.Set.Server_Port (Config, 8080);
+      AWS.Config.Set.Server_Port (Config, Port);
       AWS.Config.Set.Max_Connection (Config, 8);
       AWS.Config.Set.Accept_Queue_Size (Config, 512);
    end Configure;
@@ -25,13 +31,15 @@ begin
    Props.Load_Properties (CONFIG_PATH);
    Util.Log.Loggers.Initialize (Props);
 
+   Port := Integer_Property.Get (Props, "swagger.port", Port);
    App.Configure (Props);
    TestAPI.Servers.Server_Impl.Register (App);
 
    WS.Configure (Configure'Access);
    WS.Register_Application ("/v1", App'Unchecked_Access);
    App.Dump_Routes (Util.Log.INFO_LEVEL);
-   Log.Info ("Connect you browser to: http://localhost:8080/v1/ui/index.html");
+   Log.Info ("Connect you browser to: http://localhost:{0}/v1/ui/index.html",
+             Util.Strings.Image (Port));
 
    WS.Start;
 
