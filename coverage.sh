89d63f41b7585081215e8ba675db57d63c959171
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-15 14:27:42 +0100

    Revert to avoid using --include and --exclude option since old version of lcov do not support these options

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 NAME=openapi.cov
-lcov --quiet --base-directory . --directory . -c --include "*/openapi-ada/src/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c -o $NAME
+lcov --quiet --remove $NAME "/usr*" -o $NAME
+lcov --quiet --remove $NAME "/build*" -o $NAME
+lcov --quiet --remove $NAME "/opt*" -o $NAME
+lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
+lcov --quiet --remove $NAME "*/regtests*" -o $NAME
+lcov --quiet --remove $NAME "*/b__*" -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
