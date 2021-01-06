95714e5369a45b65bb49e9c85a98ed3b319a2ecc
persan <per.s.sandberg@bahnhof.se>
2021-01-06 09:42:58 +0100

    Handle compiler warnings

--- regtests/client/src/client/testapi-clients.ads
+++ regtests/client/src/client/testapi-clients.ads
@@ -14,7 +14,7 @@ package TestAPI.Clients is
 
    type Client_Type is new Swagger.Clients.Client_Type with null record;
 
-   --  
+   --
    --  Query an orchestrated service instance
    procedure Orch_Store
       (Client : in out Client_Type;
