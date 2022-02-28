a6157421edc0d9b2d169bc3ceb0ba6e8c0d17ee5
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-02-28 11:30:43 +0100

    Rename package Swagger.Credentials.OAuth into OpenAPI.Credentials.OAuth

--- src/openapi-credentials-oauth.adb
+++ src/openapi-credentials-oauth.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  swagger-credentials-oauth -- OAuth2 client credentials
---  Copyright (C) 2018 Stephane Carrez
+--  openapi-credentials-oauth -- OAuth2 client credentials
+--  Copyright (C) 2018, 2022 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,36 +15,42 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Util.Http.Clients;
-with Security.OAuth.Clients;
 
-package Swagger.Credentials.OAuth is
+package body OpenAPI.Credentials.OAuth is
 
-   type OAuth2_Credential_Type is new Security.OAuth.Clients.Application
-     and Credential_Type with private;
+   use Ada.Strings.Unbounded;
 
+   --  ------------------------------
    --  Set the credentials on the HTTP client request before doing the call.
+   --  ------------------------------
    overriding
    procedure Set_Credentials (Credential : in OAuth2_Credential_Type;
-                              Into       : in out Util.Http.Clients.Client'Class);
+                              Into       : in out Util.Http.Clients.Client'Class) is
+   begin
+      Into.Set_Header (Name  => "Authorization",
+                       Value => "Bearer " & Credential.Token.Get_Name);
+   end Set_Credentials;
 
+   --  ------------------------------
    --  Request a OAuth token with username and password credential.
    --  Upon successful completion, the credential contains an access token that
    --  can be used to authorize REST operations.
+   --  ------------------------------
    procedure Request_Token (Credential : in out OAuth2_Credential_Type;
                             Username   : in String;
                             Password   : in String;
-                            Scope      : in String);
+                            Scope      : in String) is
+   begin
+      Credential.Scope := To_Unbounded_String (Scope);
+      Credential.Request_Token (Username, Password, Scope, Credential.Token);
+   end Request_Token;
 
+   --  ------------------------------
    --  Refresh the OAuth access token with the refresh token.
-   procedure Refresh_Token (Credential : in out OAuth2_Credential_Type);
+   --  ------------------------------
+   procedure Refresh_Token (Credential : in out OAuth2_Credential_Type) is
+   begin
+      Credential.Refresh_Token (To_String (Credential.Scope), Credential.Token);
+   end Refresh_Token;
 
-private
-
-   type OAuth2_Credential_Type is new Security.OAuth.Clients.Application
-     and Credential_Type with record
-      Token  : Security.OAuth.Clients.Grant_Type;
-      Scope  : Ada.Strings.Unbounded.Unbounded_String;
-   end record;
-
-end Swagger.Credentials.OAuth;
+end OpenAPI.Credentials.OAuth;
