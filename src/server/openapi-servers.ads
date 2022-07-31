29261a3d5178cd997e5aee41a71cecb3a9398873
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-07-31 17:51:49 +0200

    Remove unused package

--- src/server/openapi-servers.ads
+++ src/server/openapi-servers.ads
@@ -15,7 +15,6 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Util.Http.Mimes;
 with Servlet.Rest;
 with Servlet.Core;
 with Security.Permissions;
