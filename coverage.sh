aa2218a65cd552852c4e91d816e7b2aaec4c0574
Stephane Carrez <Stephane.Carrez@gmail.com>
2024-08-22 10:18:21 +0200

    Update to run lcov from alr to get the gcov tool

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 NAME=openapi.cov
-lcov --quiet --base-directory . --directory . -c -o $NAME
-lcov --quiet --remove $NAME "/usr*" -o $NAME
-lcov --quiet --remove $NAME "/build*" -o $NAME
-lcov --quiet --remove $NAME "/opt*" -o $NAME
-lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests*" -o $NAME
-lcov --quiet --remove $NAME "*/b__*" -o $NAME
+alr exec -- lcov --quiet --base-directory . --directory . \
+   --no-external \
+   --exclude '*/b__*.adb' \
+   --exclude '*/samples/*' \
+   --exclude '*/regtests*'  -c -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
